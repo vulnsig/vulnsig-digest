133180566e790f55aa3cca7764551484cc383ecb
Christopher Ariza <ariza@flexatone.com>
2026-03-12 21:14:21 -0700

    added lint

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+  {
+    ignores: [
+      "dist/",
+      "node_modules/",
+      ".react-email/",
+      "preview/snapshot.json",
+    ],
+  },
+);
