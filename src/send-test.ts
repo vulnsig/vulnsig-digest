d8c14cb708499dad18bc50a14c5487cc4bcf5201
Christopher Ariza <ariza@flexatone.com>
2026-03-17 18:45:30 -0700

    format

--- src/send-test.ts
+++ src/send-test.ts
@@ -1,7 +1,8 @@
 import "dotenv/config";
+import type { ScheduledEvent } from "aws-lambda";
 import { handler } from "./handler.js";
 
-handler({} as any).catch((err) => {
+handler({} as unknown as ScheduledEvent).catch((err) => {
   console.error(err);
   process.exit(1);
 });
